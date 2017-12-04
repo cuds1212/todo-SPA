0825ad3abb27bf6c898440ad5881bafe668c1965
cuds1212 <cuds1212@gmail.com>
2017-12-04 15:29:23 -0800

    Turned off Mongoose debug mode.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.set("debug", true);
+//mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGO_URL, {useMongoClient: true});
